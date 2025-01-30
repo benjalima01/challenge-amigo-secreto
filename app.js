6a047616e939d6bd5c817b003c87dcc6c1a3a46d
Benjamin Lima <benjalima606@gmail.com>
2025-01-30 18:05:08 -0300

    actualice app.js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+/* Verifica que el nombre no esté vacío antes de agregarse y actualiza la lista visual.*/
+ 
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (nombreAmigo.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombreAmigo);
+        document.querySelector("amigo").value = "";
+        mostrarListaAmigo();
+    }
+}
+
+
+function mostrarListaAmigo() {
+    let listaAmigos = document.getElementById("#listaAmigos");
+    listaAmigos.innerHTML = "";
+
+
+for (let index = 0; index < amigos.length; index++) {
+        const element = amigos[index];
+    
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML); 
+    }       
+}
